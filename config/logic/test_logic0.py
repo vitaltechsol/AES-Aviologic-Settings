3f009cce751f3d71ec76b3e8b42a853bb380d058
Fabian Betancur <info@vts.us>
2024-03-29 15:05:25 -0700

    Configs

--- config/logic/test_logic0.py
+++ config/logic/test_logic0.py
@@ -18,23 +18,26 @@ class Logic:
         flaps_y = [0.0, 0.644, 1.309, 1.922, 2.56, 3.115, 3.655, 4.178, 4.696]
         # Flaps Prosim Values
         flaps_x = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+        self.flpInpl = interpolate.interp1d(flaps_x, flaps_y, kind="linear")
 
         # Rudder Angles
         rudder_y = [-0.9, -0.8, -0.5, -0.3, -0.03, 0.3, 0.5, 0.8, 0.9]
         # Rudder prosim Values
         rudder_x = [-17.0, -15.0, -10.0, -5.0, 0, 5.0, 10.0, 15.0, 17.0]
-
-        self.flpInpl = interpolate.interp1d(flaps_x, flaps_y, kind="linear")
-
         self.rdsInp =  interpolate.interp1d(rudder_x, rudder_y, kind="linear")
+
+        # Pitch Angles
+        sai_roll_y = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+        # Pitch prosim Values
+        sai_roll_x = [-90.0, -70.0, -40.0, -20.0, 0, 20.0, 40.0, 70.0, 90.0]
+        self.saiRollInp =  interpolate.interp1d(sai_roll_x, sai_roll_y, kind="linear")
 
 
     async def update(self):
         self.vars.flaps_r.value = float(self.flpInpl(self.datarefs.prosim.flaps_r.value))
         self.vars.flaps_l.value = float(self.flpInpl(self.datarefs.prosim.flaps_l.value))
-
         self.vars.rudder_trim.value = float(self.rdsInp(self.datarefs.prosim.rudder_trim.value))
-
+        self.vars.sai_roll.value = float(self.saiRollInp(self.datarefs.prosim.sai_roll.value))
 
         # self.vars.flaps_r.value = self.some_counter
         # self.vars.flaps_l.value = self.some_counter
