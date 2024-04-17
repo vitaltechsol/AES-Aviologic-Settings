ba6b5f7703081399d8fb6e77b8eb76ebedb33508
Fabian Betancur <info@vts.us>
2024-04-16 22:58:48 -0700

    Update SAI

--- config/logic/test_logic0.py
+++ config/logic/test_logic0.py
@@ -12,7 +12,7 @@ class Logic:
         self.is_enable = True
 
         # Define local variables here
-        self.some_counter = 0
+        self.some_counter = 0       
 
         # Flaps Angles
         flaps_y = [0.0, 0.644, 1.309, 1.922, 2.56, 3.115, 3.655, 4.178, 4.696]
@@ -24,13 +24,19 @@ class Logic:
         rudder_y = [-0.9, -0.8, -0.5, -0.3, -0.03, 0.3, 0.5, 0.8, 0.9]
         # Rudder prosim Values
         rudder_x = [-17.0, -15.0, -10.0, -5.0, 0, 5.0, 10.0, 15.0, 17.0]
-        self.rdsInp =  interpolate.interp1d(rudder_x, rudder_y, kind="linear")
+        self.rdsInp = interpolate.interp1d(rudder_x, rudder_y, kind="linear")
+
+        # Roll Angles
+        sai_roll_y = [6, 3.15, 1.55, 0, -3]
+        # Roll prosim Values
+        sai_roll_x = [-180, -90.0, 0, 90.0, 180]
+        self.saiRollInp = interpolate.interp1d(sai_roll_x, sai_roll_y, kind="linear")
 
         # Pitch Angles
-        sai_roll_y = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+        sai_pitch_y =  [6, 3.85, 1.55, -0.5, -3]
         # Pitch prosim Values
-        sai_roll_x = [-90.0, -70.0, -40.0, -20.0, 0, 20.0, 40.0, 70.0, 90.0]
-        self.saiRollInp =  interpolate.interp1d(sai_roll_x, sai_roll_y, kind="linear")
+        sai_pitch_x = [-180, -90.0, 0, 90.0, 180]
+        self.saiPitchInp = interpolate.interp1d(sai_pitch_x, sai_pitch_y, kind="linear")
 
 
     async def update(self):
@@ -38,6 +44,8 @@ class Logic:
         self.vars.flaps_l.value = float(self.flpInpl(self.datarefs.prosim.flaps_l.value))
         self.vars.rudder_trim.value = float(self.rdsInp(self.datarefs.prosim.rudder_trim.value))
         self.vars.sai_roll.value = float(self.saiRollInp(self.datarefs.prosim.sai_roll.value))
+        self.vars.sai_pitch.value = float(self.saiPitchInp(self.datarefs.prosim.sai_pitch.value))
+
 
         # self.vars.flaps_r.value = self.some_counter
         # self.vars.flaps_l.value = self.some_counter
