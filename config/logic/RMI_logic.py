22ae79df6428e2bc3bd851a1131c8e1e4601b4d5
Fabian Betancur <info@vts.us>
2026-05-25 23:53:46 -0700

    Add vor flags

--- config/logic/RMI_logic.py
+++ config/logic/RMI_logic.py
@@ -23,6 +23,8 @@ class Logic:
 
         # Heading flag
         self.vars.rmi_irs.hdg_m.ssm = 0 if irs_aligning == True or heading_flag == True else 3
+        self.vars.vor_adf1.vor_bear.ssm = 3 # hide flag
+        self.vars.vor_adf2.vor_bear.ssm = 3 # hide flag
 
         # Set VOR and ADF bearings in degrees. These values
         # are absolute bearings to the stations, not relative to the aircraft heading.
@@ -36,10 +38,12 @@ class Logic:
             adf1_bearing = 0
         if math.isnan(vor1_bearing):
             vor1_bearing = 0
+            self.vars.vor_adf1.vor_bear.ssm = 0 # show flag
         if math.isnan(adf2_bearing):
             adf2_bearing = 0
         if math.isnan(vor2_bearing):
             vor2_bearing = 0
+            self.vars.vor_adf2.vor_bear.ssm = 0 # show flag
 
         self.vars.vor_adf1.adf_bear.value = adf1_bearing + heading
         self.vars.vor_adf1.vor_bear.value = vor1_bearing + heading
